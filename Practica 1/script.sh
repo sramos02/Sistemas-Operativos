b708df78c8a642235f4f4215bc3a405a8ec8cd99
Sergio <31887668+sramos02@users.noreply.github.com>
2019-09-30 12:16:48 +0200

    Update script.sh

--- Practica 1/script.sh
+++ Practica 1/script.sh
@@ -4,25 +4,25 @@
 #return 1 -> error
 
 #Comprobará que el programa mytar está en el directorio actual y que es ejecutable.
-if ![-f ./mytar]; then
+if [ ! -x ./mytar ]; then
 	#En caso contrario mostrará un mensaje informativo por pantalla y terminará.
 	echo Ha ocurrido un problema, el fichero mtar no existe
 	exit 1;
 fi
 
 #Comprobará si existe un directorio tmp dentro del directorio actual.
-if ![-d tmp]; then
+if [ -d tmp ]; then
 	#Si existe lo borrará incluyendo todo lo que contenga
 	rm -r tmp
 fi
 
 #Creará un nuevo directorio temporal tmp dentro del directorio actual y cambiará a este directorio
-mkdir tmp && cd /tmp
+mkdir tmp && cd ./tmp
 
 #Creará tres ficheros
-	echo "Hello World" >> file1.txt
-	head -n10 /etc/passwd >> file2.txt
-	head -c1024 /dev/urandom >> file3.dat  #Posible error AQUI no he podido comprobar la creación de los ficheros
+	echo "Hello World" > file1.txt
+	head -n10 /etc/passwd > file2.txt
+	head -c1024 /dev/urandom > file3.dat  #Posible error AQUI no he podido comprobar la creación de los ficheros
 
 	array = {file.txt, file2.txt, file3.dat}
 
@@ -35,7 +35,7 @@ cp filetar.mtar out/
 
 #Cambiará al directorio out y ejecutará el programa mytar
 cd out
-./filetar.mtar || { echo "Error ejecutando mtar"; exit 1}
+../../mytar -xf filetar.mtar || { echo "Error ejecutando mtar"; exit 1; }
 #Comprobación
 echo "Mtar creado con exito"
 
