5b300d206b16406743258ba1136d0117f60c4a0e
Sergio <31887668+sramos02@users.noreply.github.com>
2019-11-25 17:00:33 +0100

    Update my_system.c

--- Practica 3/MySystem/my_system.c
+++ Practica 3/MySystem/my_system.c
@@ -1,5 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int system(const char * command){
+	int status = 0;
+
+	pid_t child_pid;
+	child_pid = fork();
+
+	if (child_pid != 0) { wait(&status); }
+	else {
+		int n;
+
+		n = execl("/bin/bash","/bin/bash", "-c", command, NULL);
+		status = -1;
+		return n;
+	}
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
